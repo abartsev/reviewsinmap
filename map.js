2b107206727bb1c61b366e3b92a31456a69edf32
abarcev <a.barczev@yandex.ru>
2018-09-05 12:20:09 +0300

    edit2.3

--- map.js
+++ map.js
@@ -1,5 +1,6 @@
-import { name, point, message} from './index';
+
 ymaps.ready(init);
+
   var myMap;
 
 function init () {
@@ -15,23 +16,86 @@ function init () {
     
     }); 
     myMap.geoObjects.add(clusterer);
-   
+    // const btn = document.getElementById('btn');
+    //         if (btn) {
+    //             btn.addEventListener('click', function(e){
+    //                 e.preventDefault();
+    //                 const name = document.getElementById('name').value;
+    //                 const point = document.getElementById('point').value;
+    //                 const message = document.getElementById('message').value;
+    //                 console.log(e);
+    //                 console.log(name, point, message );
+    //             })
+    //         }
     myMap.events.add('click', function (e) {
         if (!myMap.balloon.isOpen()) {
             var coords = e.get('coords');//[coords[0].toPrecision(6),coords[1].toPrecision(6)].join(', ');
-            console.log(name, point, message );
-            
-           
+            ymaps.geocode(coords)
+            .then(function (res) {
+            const points = res.geoObjects.get(0).properties.get('text');
+            localStorage.point = points;
+            var BalloonContentLayout = ymaps.templateLayoutFactory.createClass(
+                '<div class="form">' +
+                    '<div class="header">'+ points +'</div>' + 
+                        '<div class="body">' + 
+                        
+                        '</div>' +
+                        '<p class="title">Ваш отзыв</p>' +
+                    '<form>' +
+                        '<div><input id="name" type="text" placeholder="Ваше имя"/></div>' +
+                        '<div><input id="point" type="text" placeholder="Укажите место" /></div>' +
+                        '<div>' +
+                            '<textarea id="message" placeholder="Поделись впечатлениями">' +
+                            ' </textarea></div>' +
+                        '<div class="button">' +
+                        '<button id="btn">Отправить</button>' +
+                        '</div>' +
+                    '</form>' +
+                '</div>', {
+                build: function () {
+                    BalloonContentLayout.superclass.build.call(this);
+                    var that = this;
+                    document.getElementById('btn').addEventListener('click', function(e){
+                        e.preventDefault();
+                        const name = document.getElementById('name').value;
+                        const point = document.getElementById('point').value;
+                        const message = document.getElementById('message').value;
+                        const body = document.querySelector('.body');
+                        const div = document.createElement('div');
+                        div.innerHTML = `<b>${name}</b> <span>${point}</span><br/><p>${message}</p>`;
+                        body.appendChild(div);
+                        console.log(that);
+                        
+                        that.onContent(name, point, message);
+                    })
+                },
 
+                clear: function () {
+                    // Выполняем действия в обратном порядке - сначала снимаем слушателя,
+                    // а потом вызываем метод clear родительского класса.
+                    BalloonContentLayout.superclass.clear.call(this);
+                },
+
+                onContent: function (name, point, message) {
+                    objMap.coords = new ymaps.Placemark(coords, {
+                        balloonContent: `<b>${name}</b> <span>${point}</span><br/><p>${message}</p>`
+                    });
+                    clusterer.add(objMap.coords);
           
-  
-            // objMap.coords = new ymaps.Placemark(coords, {
-                
-            // });
-            // clusterer.add(objMap.coords);
-          
-        //clusterer.geoObjects.add(objMap.coords);
-            //buildObj(objMap);
+                    clusterer.geoObjects.add(objMap.coords);
+                    buildObj(objMap);
+                }
+            });
+            var balloon = new ymaps.Balloon(myMap, {
+                contentLayout: BalloonContentLayout
+            });
+            balloon.options.setParent(myMap.options);
+            balloon.open(coords);
+        
+            
+        }
+        );
+            
         }
         else {
             myMap.balloon.close();
